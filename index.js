97dfb3294f0326155ece9a0af226b0cf88807bf7
Guillermo Chacón Poveda <guillermo.chacon@bootcamp-upgrade.com>
2022-10-29 16:32:20 +0200

    FIXED: Database error

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const db = require('./src/utils/database/db');
 const housesRoutes = require('./src/api/houses/houses.routes');
 const cors = require('cors');
 require('dotenv').config();
+const db = require('./src/utils/database/db');
 
 db.connectDb();
-//const DB_URL=process.env.DB_URL;
+const DB_URL=process.env.DB_URL;
 
 
 const server=express();
